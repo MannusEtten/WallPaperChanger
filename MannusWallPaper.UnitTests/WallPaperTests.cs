ab9fb087f3c2f250e70406830a202913c79e3ba1
Mannus Etten <github@mannus.nl>
2013-10-14 23:05:24 +0200

    verandering inrichting repository

---
+++ MannusWallPaper.UnitTests/WallPaperTests.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MannusWallPaper.UnitTests
+{
+    [TestClass]
+    public class WallPaperTests
+    {
+        [TestMethod]
+        public void WallPaperElement_To_String_Is_Correct()
+        {
+            var wallpaper = MannusWallPaper.MannusWallPaperConfiguration.GetConfig().WallPapers[0];
+            var result = wallpaper.ToString();
+            var expected = "company";
+            Assert.AreEqual(expected,result);
+        }
+    }
+}
